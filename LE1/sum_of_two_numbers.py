b0c8dc913463eb92db0f8bd270578ef07f9acec7
231B331 <231B331@juet.co.in>
2025-07-29 15:13:15 +0530

    Added solution to Lab Exercise 1

---
+++ LE1/sum_of_two_numbers.py
@@ -0,0 +1,10 @@
+# sum_of_two_numbers.py
+
+def add_two_numbers(a, b):
+    return a + b
+
+# Example usage
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+result = add_two_numbers(num1, num2)
+print("Sum:", result)
